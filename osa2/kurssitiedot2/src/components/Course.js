19c26401d8e68bc625826bb3d398a178b8367484
Robert <robert.j.rantanen@helsinki.fi>
2019-08-11 17:59:53 +0300

    moduuli

---
+++ osa2/kurssitiedot2/src/components/Course.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Course = ({course}) => {
+  return (
+    <div key={course.name}>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({course}) => {
+  return <h1>{course}</h1> 
+}
+
+const Total = ({parts}) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <b>total of {total} exercises</b>
+}
+  
+const Part = ({part}) => (
+  <p key={part.id}> {part.name} {part.exercises}</p>
+)
+
+const Content = ({parts}) => (
+  parts.map(part => Part(part={part}))
+)
+
+export default Course
