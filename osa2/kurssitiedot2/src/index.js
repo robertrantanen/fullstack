42874ce816258962cdb2e36eacd70002addd5210
Robert <robert.j.rantanen@helsinki.fi>
2019-08-11 17:30:51 +0300

    kurssitiedot osa2

---
+++ osa2/kurssitiedot2/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({course}) => {
+  return <h1>{course}</h1> 
+}
+
+const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+
+  return <p>yhteensä {total} tehtävää</p>
+}
+  
+
+const Part = ({part}) =>
+  <p>{part.name} {part.exercises}</p>
+
+const Content = ({parts}) => (
+  parts.map(part => Part(part={part}))
+)
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          }          
+        ]
+      }
+    
+      return (
+        <div>
+          <Course course={course} />
+        </div>
+      )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
