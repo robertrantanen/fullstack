b2c27a46aae6a25eca64cadc91944e472d797054
Robert <robert.j.rantanen@helsinki.fi>
2019-08-11 17:45:10 +0300

    kurssitiedot 9

--- osa2/kurssitiedot2/src/index.js
+++ osa2/kurssitiedot2/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 const Course = ({course}) => {
   return (
-    <div>
+    <div key={course.name}>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
@@ -30,36 +30,51 @@ const Content = ({parts}) => (
 )
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          }, 
-          {
-            name: 'testi',
-            exercises: 12,
-            id: 4
-          }           
-        ]
-      }
+    const courses = [
+        {
+          name: 'Half Stack application development',
+          parts: [
+            {
+              name: 'Fundamentals of React',
+              exercises: 10,
+              id: 1
+            },
+            {
+              name: 'Using props to pass data',
+              exercises: 7,
+              id: 2
+            },
+            {
+              name: 'State of a component',
+              exercises: 14,
+              id: 3
+            },
+            {
+              name: 'Redux',
+              exercises: 11,
+              id: 4
+            }
+          ]
+        }, 
+        {
+          name: 'Node.js',
+          parts: [
+            {
+              name: 'Routing',
+              exercises: 3,
+              id: 1
+            },
+            {
+              name: 'Middlewares',
+              exercises: 7,
+              id: 2
+            }
+          ]
+        }
+      ]
     
       return (
-        <div>
-          <Course course={course} />
-        </div>
+        courses.map(course => Course(course={course}))
       )
 }
 
