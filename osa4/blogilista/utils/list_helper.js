502fe3a6455ce157fd232fee96f53bf81be8fcbb
Robert <robert.j.rantanen@helsinki.fi>
2019-08-27 21:11:33 +0300

    4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -7,9 +7,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const mostLikes = (blogs) => {
+  const mostLiked = blogs.reduce((a, b) => a.likes > b.likes ? a : b, 0)
+  return mostLiked.title
+}
 
 
 module.exports = {
   dummy,
   totalLikes,
+  mostLikes,
 }
