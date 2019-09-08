3cce3cad44490c77d67edf62222737415592a8bb
Robert <robert.j.rantanen@helsinki.fi>
2019-09-08 15:03:43 +0300

    5.6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}> 
+      <div style={hideWhenVisible} onClick={() => setBlogVisible(true)}>
+        {blog.title} {blog.author}
+      </div>
+      <div style={showWhenVisible} onClick={() => setBlogVisible(false)}>
+        <p>{blog.title} {blog.author}</p>
+        <p>{blog.url}</p>
+        <p>{blog.likes} likes <button onClick={() => console.log("click")}>like</button></p>
+        <p>added by {blog.user.name}</p>
+      </div>
+    </div>
+)}
 
 export default Blog
